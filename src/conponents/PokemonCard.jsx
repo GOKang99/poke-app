b85cbe1f9b24930675730e87c152ad05f6a1cc6c
MacOmer <justletomer@gmail.com>
2025-01-15 11:34:20 +0900

    포켓몬 카드게임 NewGame추가

--- src/conponents/PokemonCard.jsx
+++ src/conponents/PokemonCard.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useState } from "react";
+const cardImages = [
+  { src: "/images/1.png" },
+  { src: "/images/6.png" },
+  { src: "/images/7.png" },
+  { src: "/images/143.png" },
+  { src: "/images/382.png" },
+  { src: "/images/493.png" },
+];
 
 function PokemonCard() {
-  return <div>PokemonCard</div>;
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+    console.log(cards);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-6">포켓몬 카드게임</h1>
+      <button
+        onClick={shuffleCards}
+        className="px-6 py-2 mb-8 bg-blue-500 text-white rounded shadow-lg hover:bg-blue-700 transition"
+      >
+        New Game
+      </button>
+      <div className="grid grid-cols-4 gap-4">
+        {cards.map((card) => (
+          <div
+            className="card border-2 border-gray-300 rounded-lg shadow-md"
+            key={card.id}
+          >
+            <div>
+              <img
+                className="front w-full rounded-lg"
+                src={card.src}
+                alt="card front"
+              />
+              <img
+                className="back w-full rounded-lg"
+                src="/logo.png"
+                alt="card back"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default PokemonCard;
